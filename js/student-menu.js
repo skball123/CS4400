78e1837be560169402358ef8416132dc001f5bd4
Idan Mintz <imiintz@gmail.com>
2014-07-02 16:45:45 +0200

    added professor menu

--- js/student-menu.js
+++ js/student-menu.js
@@ -25,18 +25,7 @@ var substringMatcher = function(strs) {
     cb(matches);
   };
 };
-/*
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
-*/ 
+
 $('#dropdown .typeahead').typeahead({
   hint: true,
   highlight: true,
@@ -46,4 +35,11 @@ $('#dropdown .typeahead').typeahead({
   name: 'states',
   displayKey: 'value',
   source: substringMatcher(states)
+  templates: {
+  	empty: [
+  		'<div class="empty-message">',
+      	'No classes match your current query',
+      	'</div>'
+  	].join('\n')
+  }
 });
