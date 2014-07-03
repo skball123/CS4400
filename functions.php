a872f34431064b76f835d8a7e732e8c512b9a1ca
Sam Kirsch <sammacjunkie@gmail.com>
2014-07-03 12:11:07 +0200

    updated functions

--- functions.php
+++ functions.php
@@ -37,13 +37,40 @@ function FetchClasses() {
 	}
 	$school_num = substr_replace($school_num ,"]",-1);
 	return $school_num;
-}
-
-
-function closedb() {
 	mysqli_close($con);
 }
 
+function CheckProfEval($tut_gtid, $profgtid) {
+	$con = mysqli_connect("localhost","kirsch_cs4400","cs4400GT","kirsch_cs4400");
+	// Check connection
+	if (mysqli_connect_errno())
+  		{
+  			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+	$query = "SELECT TutoGT_ID FROM Recommends WHERE ProfGT_ID ='$profgtid' AND TutoGT_ID ='$tut_gtid'";
+	$count=mysqli_num_rows($result);
+	if($count) { return true; }
+	else { return false; }
+
+}
+
+function ProfEval($bool, $desc_eval, $num_eval, $tut_gtid, $profgtid) {	//takes in bool for choice of update or insert record
+	$con = mysqli_connect("localhost","kirsch_cs4400","cs4400GT","kirsch_cs4400");
+	// Check connection
+	if (mysqli_connect_errno())
+  		{
+  			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+	if($bool) {	
+	$query = "UPDATE Recommends SET Desc_Evaluation = '$desc_eval', Num_Evaluation = '$num_eval' WHERE TutoGT_ID = '$tut_gtid' AND ProfGT_ID = '$profgtid'";
+	$result = mysqli_query($con, $query);	
+	}
+	else {
+	$query = "INSET INTO Recommends VALUES ($profgtid, $tut_gtid, $desc_eval, $num_eval";
+	$result = mysqli_query($con, $query);
+	
+	}
+}
 
 
 
